955dd9f8473bdad1adc2ae0dd73747382de07ebf
Johannes Sluis <joesluis51@gmail.com>
2025-04-05 16:34:58 -0700

    added topdown solution

--- dynammic_programming/tribonnaci-1137.py
+++ dynammic_programming/tribonnaci-1137.py
@@ -11,3 +11,17 @@ class Solution:
 # runtime: O(n)
 # spacetime: O(n)
 # notes: range() in python does not include n so we need to go to n+1
+
+
+
+# the topdown solution
+# class Solution:
+#     def tribonacci(self, n: int) -> int:
+#         dp = {0: 0, 1: 1, 2: 1}
+#         def dfs(i):
+#             if i in dp:
+#                 return dp[i]
+#             dp[i] = dfs(i - 1) + dfs(i - 2) + dfs(i - 3)
+#             return dp[i]
+#         
+#         return dfs(n)
